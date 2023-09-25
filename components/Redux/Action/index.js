cec2d7edac41bd610925ba77c1ba7bf93952c2fd
AsHansen88 <Ande970s@stud.kea.dk>
2023-09-25 13:14:49 +0200

    :)

--- components/Redux/Action/index.js
+++ components/Redux/Action/index.js
@@ -1,31 +1,26 @@
-import { USER_STATE_CHANGE } from '../constants';
-import firebase from '../../../firebase';
-import { auth } from '../../../firebase'; 
+import { USER_STATE_CHANGE } from '../constants/index';
+import { auth, firestore } from '../../../firebase'; // Import auth and firestore from your Firebase configuration
+import { collection, doc, getDoc } from 'firebase/firestore'; // Import Firestore functions
 
 export function fetchUser() {
-  return (dispatch) => {
-    
-    const auth = firebase.auth(); 
-    const currentUser = auth.currentUser;
-    console.log('Current User:', currentUser);
+  return async (dispatch) => {
+    const user = auth.currentUser; // Access the current user from Firebase auth
 
-    if (currentUser) {
-      firebase
-        .firestore()
-        .collection("user")
-        .doc(currentUser.uid)
-        .get()
-        .then((snapshot) => {
-          if (snapshot.exists) {
-            console.log(snapshot.data());
-            dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data() });
-          } else {
-            console.log('User data does not exist');
-          }
-        })
-        .catch((error) => {
-          console.error('Error fetching user data:', error);
-        });
+    if (user) {
+      const userDocRef = doc(firestore, 'user', user.uid);
+
+      try {
+        const snapshot = await getDoc(userDocRef);
+
+        if (snapshot.exists()) {
+          console.log(snapshot.data());
+          dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data() });
+        } else {
+          console.log('User data does not exist');
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
     } else {
       console.log('No user is currently logged in');
       // You may want to dispatch an action here to handle the case where no user is logged in.
