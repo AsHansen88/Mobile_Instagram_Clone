e7de71c78fba3f60511081d2f6548082d8714699
AsHansen88 <Ande970s@stud.kea.dk>
2023-09-25 16:34:30 +0200

    :)

--- components/Main/Add.js
+++ components/Main/Add.js
@@ -1,28 +1,50 @@
 import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [hasCameraPermission, setHasCameraPermissions] = useState(null);
+
+  useEffect(async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    setHasCameraPermissions(status === 'granted');
+
+    const imagePickerStatus = await ImagePicker.requestMediaLibraryPermissionsAsync(); // Use requestMediaLibraryPermissionsAsync for image picker
+    setHasPermissions(imagePickerStatus.status === 'granted');
+  }, []);
 
   if (!permission) {
     // Camera permissions are still loading
     return <View />;
   }
 
-  if (!permission.granted) {
+  const PickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true, 
+      aspect: [1, 1],
+      quality: 1,
+    });
+    console.log(result);
+  };
+
+  if (!hasCameraPermission) {
     // Camera permissions are not granted yet
     return (
       <View style={styles.container}>
         <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
+        <Button onPress={requestPermission} title="Grant permission" />
       </View>
     );
   }
 
   function toggleCameraType() {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
   }
 
   return (
@@ -32,6 +54,11 @@ export default function App() {
           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
             <Text style={styles.text}>Flip Camera</Text>
           </TouchableOpacity>
+          <Button
+          title="Pick Image from Gallery"
+           onPress={() => PickImage()}
+           style={styles.button} // Add this style prop
+/>
         </View>
       </Camera>
     </View>
@@ -52,10 +79,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
     margin: 64,
   },
-  button: {
+  button: { 
     flex: 1,
     alignSelf: 'flex-end',
     alignItems: 'center',
+    width: 10,
+    height: 10, // Adjust the height as needed
   },
   text: {
     fontSize: 24,
@@ -63,3 +92,4 @@ const styles = StyleSheet.create({
     color: 'white',
   },
 });
+
