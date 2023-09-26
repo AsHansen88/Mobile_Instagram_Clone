c2ee5e59fb68f21bcad0104c89b5334abe715845
AsHansen88 <Ande970s@stud.kea.dk>
2023-09-26 17:45:59 +0200

    :)

--- components/Main/Add.js
+++ components/Main/Add.js
@@ -2,31 +2,51 @@ import { Camera, CameraType } from 'expo-camera';
 import { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+//import  firebase from '../../firebase/storage'; 
+import { storage } from '../../firebase'; 
 
-export default function Add(Navigation) {
+export default function Add({ navigation }) {
   const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
-  const [hasCameraPermission, setHasCameraPermissions] = useState(null)
-  const [camera, setCamera] = useState(null)
-  const [image, setImage] = useState(null)
-  const [type, setType] = useState(Camera.Constants.Type.back)
+  const [hasCameraPermission, setHasCameraPermissions] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
 
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermissions(cameraStatus.status === 'granted');
 
-  useEffect(async () => {
-    const cameraStatus = await Camera.requestCameraPermissionsAsync();
-    setHasCameraPermissions(cameraStatus.status === 'granted');
-
-    const GalleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        setHasGalleryPermission(GalleryStatus.status === 'granted');
+      const GalleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(GalleryStatus.status === 'granted');
+    })();
   }, []);
 
   const takePicture = async () => {
-    if (camera){
-      const data = await camera.takePictureAsync(null)
-      console.log(data.uri)
+    if (camera) {
+      const photo = await camera.takePictureAsync();
+      uploadImageToFirebase(photo.uri);
     }
-  }
+  };
 
- const PickImage = async () => {
+  const uploadImageToFirebase = async (uri) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+  
+    const storageRef = storage.ref().child('images/' + new Date().getTime() + '.jpg');
+  
+    try {
+      await storageRef.put(blob);
+      const downloadURL = await storageRef.getDownloadURL();
+      setImage(downloadURL); 
+      console.log('Image uploaded to Firebase:', downloadURL);
+    } catch (error) {
+      console.error('Error uploading image to Firebase:', error);
+    }
+  };
+  
+
+  const PickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
@@ -34,7 +54,7 @@ export default function Add(Navigation) {
       quality: 1,
     });
   
-    if (!result.cancelled) {
+    if (!result.canceled) {
       setImage(result.uri);
     }
   };
@@ -61,7 +81,7 @@ export default function Add(Navigation) {
   return (
     <View style={styles.container}>
       <View style={styles.cameraContainer}>
-        <Camera style={styles.camera} type={type}>
+        <Camera style={styles.camera} type={type} ratio={'1:1'} ref={(ref) => setCamera(ref)}>
           <View style={styles.buttonContainer}>
             <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
               <Text style={styles.text}>Flip Camera</Text>
@@ -69,18 +89,23 @@ export default function Add(Navigation) {
           </View>
         </Camera>
       </View>
+
+      <View style={styles.imageContainer}>
+        {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
+      </View>
+
       <View style={styles.buttonContainer}>
+        <Button title="Take Picture" onPress={takePicture} />
+
         <Button
           title="Pick Image from Gallery"
           onPress={() => PickImage()}
           style={styles.pickImageButton}
         />
-        <Button
-          title="Take picture" onPress={() => takePicture() }/>
-          {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
-          <Button
-          title="Save" onPress={() => Navigation.navigate('Save', { image })}/>
-
+         <Button
+  title="Save"
+  onPress={() => uploadImageToFirebase('Save', {image})} 
+/>
 
       </View>
     </View>
@@ -97,6 +122,9 @@ const styles = StyleSheet.create({
   camera: {
     flex: 1,
   },
+  imageContainer: {
+    flex: 1,
+  },
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
@@ -111,8 +139,5 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: 'white',
-  },
-  pickImageButton: {
-    margin: 16,
   },
 });
