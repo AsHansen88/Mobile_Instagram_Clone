6b4314b73dac8145cf9e1f3754e6e2bcbbe3190d
AsHansen88 <Ande970s@stud.kea.dk>
2023-09-27 21:02:26 +0200

    :)

--- components/Main/Profile.js
+++ components/Main/Profile.js
@@ -1,70 +1,72 @@
-import React from 'react'
-import { View, Text, Image, FlatList, StyleSheet } from 'react-native'
-import { connect } from 'react-redux'
-
+import React from 'react';
+import { View, Text, Image, FlatList, StyleSheet, Button } from 'react-native';
+import { connect } from 'react-redux';
+import { auth } from '../../firebase';  // Assuming you have Firebase initialized
+import { useNavigation } from '@react-navigation/native';
 
 function Profile(props) {
-  const { currentUser, posts } = props;
+  const navigation = useNavigation(); // Hook for navigation
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut(); // Use auth.signOut() to log out the user
+      // Navigate to the Login screen
+      navigation.navigate('Login');
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
+
+  const { currentUser, posts } = props; // Access currentUser via props
 
   return (
-    <View styles= { styles.container}>
-      <View styles= { styles.containerInfo}>
-        <View styles= {styles.containerGallery}>
-      <Text>
-        Profile
-      </Text>
-      {currentUser && currentUser.name && currentUser.email &&(
-        <View>
-        <Text>User Name:</Text>
-        <Text>{currentUser.name}</Text>
-        <Text>User Email:</Text>
-        <Text>{currentUser.email}</Text>
-    
-    </View>
-      )}
-    </View>
-    </View>
-    <FlatList 
-    numberColumns={3}
-    horizontal ={false}
-    data={posts}
-    renderItem={({item}) => (
-    
-    <Image 
-          style={styles.Image}
-          source = {{uri: item.downloadURL}}
-      />
-    
-      )}
-    />
+    <View style={styles.container}>
+      <View style={styles.containerInfo}>
+        <Text>Profile</Text>
+        {currentUser && currentUser.name && currentUser.email && (
+          <View>
+            <Text>User Name:</Text>
+            <Text>{currentUser.name}</Text>
+            <Text>User Email:</Text>
+            <Text>{currentUser.email}</Text>
+          </View>
+        )}
+      </View>
 
+      <FlatList
+        numColumns={3}
+        horizontal={false}
+        data={posts}
+        renderItem={({ item }) => (
+          <Image
+            style={styles.Image}
+            source={{ uri: item.downloadURL }}
+          />
+        )}
+      />
+
+      <Button title="Logout" onPress={handleLogout} />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 40
+    marginTop: 40,
   },
   containerInfo: {
     margin: 20,
   },
-  containerGallery: {
-    flex: 1
-  },
   Image: {
     flex: 1,
-    aspectRatio: 1/1
+    aspectRatio: 1 / 1,
   },
-  containerImage: {
-    flex: 1/3
-  }
-}) 
+});
 
 const mapStateToProps = (store) => ({
   currentUser: store.userState.currentUser,
   posts: store.userState.posts,
-})
+});
 
-export default connect(mapStateToProps, null)(Profile)
+export default connect(mapStateToProps, null)(Profile);
